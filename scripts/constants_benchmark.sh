710d47cf2c7d1fb0e23fbb486f893924ba8c6def
Kenje Hofileña <kenje4090@gmail.com>
2021-04-29 18:54:07 +0800

    Update constants benchmark script

--- scripts/constants_benchmark.sh
+++ scripts/constants_benchmark.sh
@@ -4,7 +4,7 @@
 set -m
   
 # Start worker and put it in background
-go run ./cxexecutes/worker/cmd/server.go -workers=3 &
+./server -workers=3 &
   
 # Start the main process
-go run main.go --constants=true --rounds=10 --name=Constants --population=300 --generations=1000 --expressions=30 --graphs=true --ast=true --use-log-fitness=false --workers=3
+./cx-evolves --constants=true --rounds=10 --name=Constants --population=300 --generations=1000 --expressions=30 --graphs=true --ast=true --use-log-fitness=false --workers=3
