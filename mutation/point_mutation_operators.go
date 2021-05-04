896eb80d0172e44e79a2c3a5bdf4fc2f01ecb539
Kenje Hofileña <kenje4090@gmail.com>
2021-05-04 14:21:05 +0800

    Add more point mutation operators

--- mutation/point_mutation_operators.go
+++ mutation/point_mutation_operators.go
@@ -3,6 +3,8 @@ package mutation
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
+	"math/rand"
 
 	cxast "github.com/skycoin/cx/cx/ast"
 	cxconstants "github.com/skycoin/cx/cx/constants"
@@ -56,8 +58,10 @@ func HalfI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxa
 
 	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
 
+	fmt.Printf("value=%v\n", binary.LittleEndian.Uint32(currentByts))
 	// Divide the value by 2
 	newValue := (binary.LittleEndian.Uint32(currentByts)) / 2
+	fmt.Printf("new value=%v\n", newValue)
 
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.LittleEndian, newValue)
@@ -182,6 +186,46 @@ func AddOneI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *c
 	}
 }
 
+// AddRand1ByteLiteral adds a random 1 byte to the existing literal value.
+func AddRand1ByteI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	gen1Byt, err := GenerateRandomBytes(1)
+	if err != nil {
+		panic(err)
+	}
+	randomInt8 := binary.LittleEndian.Uint32(gen1Byt)
+
+	newValue := (binary.LittleEndian.Uint32(currentByts)) + randomInt8
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
 // SubOneiteral substracts an i32 literal value with 1.
 func SubOneI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
 	argOffset := cxarg.Offset
@@ -195,6 +239,438 @@ func SubOneI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *c
 
 	// Subtract 1 to the value
 	newValue := (binary.LittleEndian.Uint32(currentByts)) - 1
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// SubRand1ByteLiteral subtracts a random 1 byte to the existing literal value.
+func SubRand1ByteI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	gen1Byt, err := GenerateRandomBytes(1)
+	if err != nil {
+		panic(err)
+	}
+	randomInt8 := binary.LittleEndian.Uint32(gen1Byt)
+
+	newValue := (binary.LittleEndian.Uint32(currentByts)) - randomInt8
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// BitOrI32Literal sets random nth bit to 1.
+func BitOrI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+
+	gen1Byt, err := GenerateRandomBytes(1)
+	if err != nil {
+		panic(err)
+	}
+	randomInt8 := binary.LittleEndian.Uint32(gen1Byt)
+
+	// Sets random nth bit to 1.
+	newValue := currVal | (1 << (randomInt8 % 32))
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// BitAndI32Literal sets all bits to zero except for random nth bit.
+func BitAndI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+
+	gen1Byt, err := GenerateRandomBytes(1)
+	if err != nil {
+		panic(err)
+	}
+	randomInt8 := binary.LittleEndian.Uint32(gen1Byt)
+
+	// Sets rall bits to 0 except for random nth bit.
+	newValue := currVal & (1 << (randomInt8 % 32))
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// BitXorI32Literal flips random nth bit.
+func BitXorI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+
+	gen1Byt, err := GenerateRandomBytes(1)
+	if err != nil {
+		panic(err)
+	}
+	randomInt8 := binary.LittleEndian.Uint32(gen1Byt)
+
+	// Sets random nth bit to 1.
+	newValue := currVal ^ (1 << (randomInt8 % 32))
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// BitRotateLeftI32Literal rotates whole 32 byte to the left.
+func BitRotateLeftI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+
+	gen1Byt, err := GenerateRandomBytes(1)
+	if err != nil {
+		panic(err)
+	}
+	randomInt8 := binary.LittleEndian.Uint32(gen1Byt)
+
+	// Rotate whole 32bit with rand
+	randRotate := randomInt8 % 32
+	newValue := (currVal << randRotate) | (currVal >> (32 - randRotate))
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// BitRotateRightI32Literal rotates whole 32 byte to the right.
+func BitRotateRightI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+
+	gen1Byt, err := GenerateRandomBytes(1)
+	if err != nil {
+		panic(err)
+	}
+	randomInt8 := binary.LittleEndian.Uint32(gen1Byt)
+
+	// Rotate whole 32bit with rand
+	randRotate := randomInt8 % 32
+	newValue := (currVal >> randRotate) | (currVal << (32 - randRotate))
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// OrI32Literal does OR operation on the literal value and random uint32 value.
+func OrI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+	newValue := currVal | rand.Uint32()
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// AndI32Literal does AND operation on the literal value and random uint32 value.
+func AndI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+	newValue := currVal & rand.Uint32()
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// XorI32Literal does XOR operation on the literal value and random uint32 value.
+func XorI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+	newValue := currVal ^ rand.Uint32()
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// ShiftOneBitLeft shifts whole ltieral value one bit to left.
+func ShiftOneBitLeftI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+	newValue := currVal << 1
+
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, newValue)
+
+	newByts := buf.Bytes()
+
+	lenCurrByts := len(currentByts)
+	lenNewByts := len(newByts)
+	if lenCurrByts > lenNewByts {
+		diff := lenCurrByts - lenNewByts
+		for i := 0; i < diff; i++ {
+			newByts = append(newByts, byte(0))
+		}
+	}
+
+	// Overwrite current value with new value
+	x := 0
+	for i := argOffset; i < argOffset+cxarg.TotalSize; i++ {
+		cxprogram.Memory[i] = newByts[x]
+		x++
+	}
+}
+
+// ShiftOneBitRight shifts whole ltieral value one bit to right.
+func ShiftOneBitRightI32Literal(cxprogram *cxast.CXProgram, pkg *cxast.CXPackage, cxarg *cxast.CXArgument) {
+	argOffset := cxarg.Offset
+
+	// Check if has value in data segment memory
+	if argOffset == 0 {
+		return
+	}
+
+	currentByts := cxprogram.Memory[argOffset : argOffset+cxarg.TotalSize]
+	currVal := binary.LittleEndian.Uint32(currentByts)
+	newValue := currVal >> 1
 
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.LittleEndian, newValue)
