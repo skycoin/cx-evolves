7b25a1fd14fd1cd917e0a7f0abc20af119adfd11
Kenje Hofileña <kenje4090@gmail.com>
2021-08-24 11:26:24 +0800

    Add new plotter method

--- plotter/plotter.go
+++ plotter/plotter.go
@@ -1,6 +1,9 @@
 package plotter
 
 import (
+	"encoding/json"
+	"os"
+
 	"gonum.org/v1/plot/plotter"
 )
 
@@ -25,5 +28,71 @@ func UpdateDataPoints(plotData *PlotData, generation int, output []float64, save
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func AddTitleToJSON(title, saveDirectory string) error {
+	var plotData PlotData
+	plotData.Title = title
+
+	data, err := json.MarshalIndent(plotData, "", " ")
+	if err != nil {
+		return err
+	}
+
+	data = append(data[0:len(data)-6], []byte("[")...)
+	err = AppendToFile(string(data), saveDirectory)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func AddDataToJSON(dataPoints PlotDataPoints, saveDirectory string) error {
+	data, err := json.MarshalIndent(dataPoints, "", " ")
+	if err != nil {
+		return err
+	}
+
+	dataStr := "\n{" + string(data[1:len(data)-2]) + "\n}"
+	err = AppendToFile(string(dataStr), saveDirectory)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func AddCommaToJSON(saveDirectory string) error {
+	data := ","
+	err := AppendToFile(string(data), saveDirectory)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func AddClosingToJSON(saveDirectory string) error {
+	data := "\n]\n}"
+	err := AppendToFile(string(data), saveDirectory)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func AppendToFile(data string, saveDir string) error {
+	f, err := os.OpenFile(saveDir, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	if _, err = f.WriteString(data); err != nil {
+		return err
+	}
+
 	return nil
 }
